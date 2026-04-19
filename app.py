2fcd5f5008cfa0c88d9d5bfe6c4c60b1aaeac48a
urk23cs1165 <poornas@karunya.edu.in>
2026-04-19 17:09:55 +0530

    inital commit

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app=Flask(__name__)
 @app.route("/")
 def home():
-    return "Docker is running"
+    return "Docker is runninggg"
 if __name__=='__main__':
     app.run(host='0.0.0.0',port=5000)
